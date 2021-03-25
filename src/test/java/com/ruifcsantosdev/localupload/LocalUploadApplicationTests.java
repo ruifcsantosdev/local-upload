2b4720b926c49a89aba3cdbc21c283ea579f71e0
ruifcsantosdev <ruifcsantosdev@outlook.com>
2021-03-25 14:24:02 +0000

    first commit

---
+++ src/test/java/com/ruifcsantosdev/localupload/LocalUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruifcsantosdev.localupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
